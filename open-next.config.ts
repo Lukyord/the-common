86fce03fb4a90a83bd78979574f2420fbd1a4c71
Kongtup <ttansiri524@gmail.com>
2026-06-22 09:17:49 +0700

    chores: add data cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,4 +1,9 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from '@opennextjs/cloudflare/config'
+import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache'
+import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache'
 
-export default defineCloudflareConfig({})
+export default defineCloudflareConfig({
+  incrementalCache: withRegionalCache(r2IncrementalCache, {
+    mode: 'long-lived',
+  }),
+})
